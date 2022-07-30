0c1ce96a4fba0eefd2ddec0c532877dc66b2856c
RichardHpa <richard.m.hpa@gmail.com>
2022-07-30 14:38:10 +1200

    set up dev env

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// JS Goes Here
+$(document).ready(function () {
+  $('#hamburger').click(function () {
+    $('#navigation').toggle();
+  });
+});
+
+// Script to hide/show menu
+const button = document.querySelector('#hamburger');
+let menu = document.querySelector('#navigation');
+button.addEventListener('click', function (event) {
+  if (menu.style.display == 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+});
+
+$(document).ready(function () {
+  $('#closeButton').click(function () {
+    $('#dialog').addClass('hideDialog');
+  });
+
+  $('#learnMoreButton').click(function () {
+    $('#dialog').removeClass('hideDialog');
+  });
+});
